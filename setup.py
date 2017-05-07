a3382c961058a4bf0e7e3b94d3f6e46d1cb9e4e6
Lukas Kollmer <lukas.kollmer@gmail.com>
2017-05-07 20:14:52 +0200

    initial commit

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name="touchid",
+    py_modules=["touchid"],
+    version="1.0.0",
+    description="Access the Touch-ID sensor",
+    long_description="`touchid` provides an easy to use API for accessing the Touch-ID sensor via Apple's LocalAuthentication framework",
+    keywords=["touch-id", "LocalAuthentication", "LAContext"],
+    license="MIT",
+    author="Lukas Kollmer",
+    author_email="lukas.kollmer@gmail.com",
+    url="https://github.com/lukaskollmer/py-touch-id/"
+)
---
+++ touchid.py
@@ -0,0 +1,52 @@
+import sys
+import ctypes
+import objc
+from LocalAuthentication import LAContext, LAPolicyDeviceOwnerAuthenticationWithBiometrics
+
+c = ctypes.cdll.LoadLibrary(None)
+DISPATCH_TIME_FOREVER = sys.maxint
+
+dispatch_semaphore_create = c.dispatch_semaphore_create
+dispatch_semaphore_create.restype = ctypes.c_void_p
+dispatch_semaphore_create.argtypes = [ctypes.c_int]
+
+dispatch_semaphore_wait = c.dispatch_semaphore_wait
+dispatch_semaphore_wait.restype = ctypes.c_long
+dispatch_semaphore_wait.argtypes = [ctypes.c_void_p, ctypes.c_uint64]
+
+dispatch_semaphore_signal = c.dispatch_semaphore_signal
+dispatch_semaphore_signal.restype = ctypes.c_long
+dispatch_semaphore_signal.argtypes = [ctypes.c_void_p]
+
+def authenticate(reason='authenticate via Touch-ID'):
+    context = LAContext.new()
+
+    can_evaluate = context.canEvaluatePolicy_error_(LAPolicyDeviceOwnerAuthenticationWithBiometrics, None)[0]
+    if not can_evaluate:
+        raise Exception('Touch-ID doesn\'t seem to be available on this machine')
+
+    sema = dispatch_semaphore_create(0)
+
+    # we can't reassign objects from another scope, but we can modify them
+    res = {'success': False, 'error': None}
+
+    def callback(_success, _error):
+        res['success'] = _success
+        if _error:
+            res['error'] = _error.localizedDescription()
+        dispatch_semaphore_signal(sema)
+
+    context.evaluatePolicy_localizedReason_reply_(LAPolicyDeviceOwnerAuthenticationWithBiometrics, reason, callback)
+    dispatch_semaphore_wait(sema, DISPATCH_TIME_FOREVER)
+
+    if res['error']:
+        raise Exception(res['error'])
+
+    return res['success']
+
+
+if __name__ == '__main__':
+
+    s = authenticate('HEY')
+    print(s)
+
